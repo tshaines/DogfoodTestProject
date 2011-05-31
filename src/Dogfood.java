fd39d7299724240696eb0ab8bdc64a3f485fccb5
tshaines <tsh_github@squirrelfish.org>
2011-05-30 18:30:58 -0700

    no liars!

--- src/Dogfood.java
+++ src/Dogfood.java
@@ -3,6 +3,7 @@ public class Dogfood {
 
 	public static void main(String[] args) {
 		System.out.println("I like big butts and I cannot lie.");
+		System.out.println("You other brothers can't deny.");
 	}
 
 }
