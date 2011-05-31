ae92253ebb93e0253a65f9681de6599ad3bf251c
tshaines <tsh_github@squirrelfish.org>
2011-05-30 18:38:40 -0700

    Edited src/Dogfood.java via GitHub

--- src/Dogfood.java
+++ src/Dogfood.java
@@ -1,9 +1,11 @@
-
 public class Dogfood {
 
 	public static void main(String[] args) {
 		System.out.println("I like big butts and I cannot lie.");
 		System.out.println("You other brothers can't deny.");
+
+		System.out.println("my anaconda don't want none unless you got buns hun...");
+
 	}
 
 }
